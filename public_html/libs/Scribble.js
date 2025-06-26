6959fe46e241de307d5ac00e19b2863505812f30
Joji Mitsumoto <mitsumotocs@gmail.com>
2025-06-26 10:16:54 +0900

    Saved

--- public_html/libs/Scribble.js
+++ public_html/libs/Scribble.js
@@ -1,18 +1,37 @@
 export class Scribble {
     canvasElement;
     canvasContext;
+    boundResize;
 
     constructor(canvasElement) {
         this.canvasElement = canvasElement;
         this.canvasContext = this.canvasElement.getContext("2d");
         console.log(this.canvasElement, this.canvasContext);
+
+        this.boundResize = this.resize.bind(this);
+        
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+    }
+
+    enable() {
+        
+    }
+
+    disable() {
+
     }
 
     resize() {
-        console.log("resizing canvas...");
-
+        const { width, height } = this.canvasElement.getBoundingClientRect();
+        console.log(`resizing canvas: W = ${width} / H = ${height}`);
+        this.canvasElement.width = width;
+        this.canvasElement.height = height;
     }
 
+    startDrawing() {
+        
+    }
 
-
+    
 }
